6cc9a211893b0d0cc1f401f207dcd22a863d2d54
Louis Taylor <louie.j.taylor@gmail.com>
2017-10-11 07:21:11 -0400

    command-line args (mostly), cleanup and refactor

---
+++ README.md
@@ -0,0 +1,15 @@
+#Komplexity
+
+A k-mer-based approach to masking low-complexity sequences.
+
+##Install
+
+    git clone https://github.com/louiejtaylor/komplexity
+
+or just [grab the binary](https://github.com/louiejtaylor/komplexity/blob/master/komplexity).
+
+##Usage
+
+    ./komplexity -in [-out] [-k] [-win]
+
+
--- komplexity.go
+++ komplexity.go
@@ -5,18 +5,15 @@ import (
 	"github.com/biogo/biogo/alphabet"
 	"github.com/biogo/biogo/io/seqio/fasta"
 	"github.com/biogo/biogo/seq"
+
 	"fmt"
 	"os"
 	"strings"
-	"math"
+//	"math"
+	"flag"
+
 //	"reflect"
 )
-
-//score by probability
-func probScorer(n, k, l int) (score float64) {
-        //TODO
-	return score
-}
 
 //score by sequence length
 func lenScorer(n, k, l int) (score float64) {
@@ -32,48 +29,58 @@ func kCounter(k int, seq string) (counts map[string]int) {
 	}
         return counts
 }
+
 //mask a sequence given a list of positions [start, end, start, end...]
 func maskSeq(sequ seq.Sequence, positions []int) (seqOut alphabet.Slice) {
 	beghq := 0
 	endhq := 0
 	seqOut = sequ.Slice().Slice(0,0)
-	fmt.Println(positions)
-	fmt.Println(sequ.Len())
-	fmt.Println(len(positions))
 	for i := 0; i < len(positions)/2; i++ {
-	//	fmt.Println(seqOut)
 		endhq = positions[i*2]
-	//	fmt.Println(lastpos)
 		seqOut = seqOut.Append(sequ.Slice().Slice(beghq,endhq))
-	//	fmt.Println(seqOut)
 		beghq = positions[i*2+1]
 		seqOut = seqOut.Append(linear.NewSeq("",[]alphabet.Letter(strings.Repeat("N", beghq - endhq)),alphabet.DNA).Slice())
-		fmt.Println(beghq, endhq)
 	}
-	fmt.Println("bef", beghq, sequ.Len())
 	seqOut = seqOut.Append(sequ.Slice().Slice(beghq, sequ.Len()))
-	fmt.Println("aft")
 	return seqOut
 }
 
 
 func main() {
-	//TODO command line input
-	infile, err := os.Open("chr2_caps.fa")
+	//grab input from command line
+	var filein string
+	flag.StringVar(&filein, "in", "test.fa", "Input filename")
+
+	//k := flag.Int("k", 4, "k-mer size")
+	k := 4
+
+	//winlen := flag.Int("win", 100, "window length")
+	winlen := 100
+	var fileout string
+	flag.StringVar(&fileout, "out", filein+"_filtered", "Output filename")
+	//fileout := filein + "_filtered"
+
+	flag.Parse()
+
+	//set up files
+	infile, err := os.Open(filein)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	defer infile.Close()
-	outfile, er := os.Create("test_chr2_.3.fa")
+	outfile, er := os.Create(fileout)
 	if er != nil {
                 fmt.Println(err)
                 os.Exit(1)
         }
 	defer outfile.Close()
-	//TODO allow choosing to filter or mask
+
+	//setup reader and writer
 	in := fasta.NewReader(infile, linear.NewSeq("", nil, alphabet.DNA))
 	out := fasta.NewWriter(outfile, 60)
+
+	//read infile
 	for {
 		s, er := in.Read()
 
@@ -81,34 +88,38 @@ func main() {
 			break
 
 		} else { //process sequence
-			//TODO variable window lengths
-			winlen := 100
-			k := 4
-			//TODO variable k
+
 			imap := kCounter(k,fmt.Sprintf("%v",s.Slice().Slice(0,winlen)))
 			iscore := lenScorer(len(imap), k, winlen)
 			fmt.Println(iscore)
-			//TODO dynamic threshold
+
 			threshold := 0.55
 			filtering := false //toggle
+
 			var filterpos []int = make([]int,0,10)
+
+			//check if initial score below threshold
 			if iscore < threshold {
 				filtering = true
 				filterpos = append(filterpos, 0)
 			}
+
 			for j := 0; j < s.Len() - winlen; j++ {
-				if math.Mod(float64(j),1000000.0) == 0 {
-					fmt.Println(j)
-				}
+				//grab first and last kmers
 				oldk := fmt.Sprintf("%v",s.Slice().Slice(j,j+k))
 				newk := fmt.Sprintf("%v",s.Slice().Slice(j+winlen-k,j+winlen))
+				//get rid of old kmer
 				if imap[oldk] == 1 {
 					delete(imap, oldk)
 				} else {
 					imap[oldk]--
 				}
+
+				//add new kmer
 				imap[newk]++
 				iscore = lenScorer(len(imap), k, winlen)
+
+				//add "inflection point" if necessary
 				if filtering {
 					if iscore > threshold {
 						filterpos = append(filterpos, j + winlen)
@@ -120,25 +131,20 @@ func main() {
 						filtering = true
 					}
 				}
-				//if math.Mod(float64(j),10.0) == 0 {
-				//	fmt.Println("%v %v", len(imap), iscore)
-				//}//fmt.Println(iscore)
 			}
+
+			//after processing, add final position
 			if filtering {
 				filterpos = append(filterpos, s.Len())
 			}
+
 			if len(filterpos) == 0 {
 				out.Write(s)
 			} else {
 				filtered := maskSeq(s, filterpos)
-				//fmt.Println(filtered)
 				out.Write(linear.NewSeq("",[]alphabet.Letter(fmt.Sprintf("%v",filtered.Slice(0,filtered.Len()))),alphabet.DNA))
 			}
-			//fmt.Println(imap)
-			//fmt.Println(filtered)
-			//fmt.Println(filterpos)
 		}
 	}
-//	outfile.Close()
 }
 
